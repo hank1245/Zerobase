0d02dce9a310772edb63088a2917f14f3e610478
김희웅 <hank29206880@gmail.com>
2022-05-22 22:12:10 +0900

    footer darkmode

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,15 @@
 import styles from './style/Footer.module.css'
 import {BsInstagram,BsFacebook,BsGithub} from 'react-icons/bs'
 import {BiCopyright} from 'react-icons/bi'
+import {useRecoilValue} from 'recoil';
+import {darkModeState} from '../atoms/darkMode';
 
 function Footer() {
+  const isDarkMode = useRecoilValue(darkModeState)
+
   return (
       <>
-        <footer className={styles.footer}>
+        <footer className={isDarkMode ? styles.footer : styles.footer_light}>
             <p>제로베이스</p>
             <ul className={styles.creditcard}>
                 <li><img src='/icons/visa.svg' alt="visa"/></li>
