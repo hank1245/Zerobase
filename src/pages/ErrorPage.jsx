0bb2cc05244afc7d23cdfff27928d5a401c83897
김희웅 <hank29206880@gmail.com>
2022-05-22 19:44:16 +0900

    Errorpage routing

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,5 +1,7 @@
 import styles from './styles/ErrorPage.module.css'
 import {Button} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
+
 
 function ErrorPage() {
   return (
@@ -7,7 +9,7 @@ function ErrorPage() {
       <div className={styles.align_center}>
         <h1>404</h1>
         <h2>페이지를 찾을 수 없습니다</h2>
-        <Button variant="primary" className='py-3 px-md-5 mt-3'>메인으로</Button>
+        <Link to='/'><Button variant="primary" className='py-3 px-md-5 mt-3'>메인으로</Button></Link>
       </div>
    </>
   )
