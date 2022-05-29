664f49ffa25e889778157e00d2db2e4f40a1d6f6
susu <isks2838@gmail.com>
2022-05-29 23:27:00 +0900

    페이지 이동시 스크롤 상단 고정

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import './App.css';
-import { Routes, Route,} from 'react-router-dom'
-import {useRecoilValue} from 'recoil';
-import {darkModeState} from './atoms/darkMode';
-import Header from './components/Header';
-import Dashboard from './pages/Dashboard';
-import CategoryPage from './pages/CategoryPage';
-import ErrorPage from './pages/ErrorPage';
-import Footer from './components/Footer';
-import ProductPage from './pages/ProductPage';
-import ShoppingCart from './components/ShoppingCart';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { darkModeState } from "./atoms/darkMode";
+import Header from "./components/Header";
+import Dashboard from "./pages/Dashboard";
+import CategoryPage from "./pages/CategoryPage";
+import ErrorPage from "./pages/ErrorPage";
+import Footer from "./components/Footer";
+import ProductPage from "./pages/ProductPage";
+import ShoppingCart from "./components/ShoppingCart";
+import ScrollToTop from "./components/ScrollRestoration";
 
 function App() {
-  const isDarkMode = useRecoilValue(darkModeState)
+  const isDarkMode = useRecoilValue(darkModeState);
 
   return (
     <>
-    <div className={isDarkMode ? 'App_dark' : 'App_light'}>
-      <Header/>
-      <Routes>
-        <Route path= '/' element={<Dashboard/>}/>
-        <Route path= '/cart' element={<ShoppingCart/>}/> 
-        <Route path= '/:category' element={<CategoryPage/>}/> 
-        <Route path="/product/:id" element={<ProductPage />} />
-        <Route path = '*' element={<ErrorPage/>}/>
-      </Routes>
-    </div>
-    <Footer/>
+      <div className={isDarkMode ? "App_dark" : "App_light"}>
+        <Header />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/cart" element={<ShoppingCart />} />
+          <Route path="/:category" element={<CategoryPage />} />
+          <Route path="/product/:id" element={<ProductPage />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </div>
+      <Footer />
     </>
   );
 }
